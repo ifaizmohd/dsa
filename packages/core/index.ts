599b769c64803497bf1d728385c0d5eb4a5a5b65
Faiz Mohd <42231693+ifaizmohd@users.noreply.github.com>
2025-01-22 22:26:24 +0530

    added merge sorted arrays

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,12 +1,12 @@
-import { maxProfit } from '../techniques/two-pointer/questions/best-time-to-buy-sell-stocks';
+import { merge } from '../techniques/two-pointer/questions/merge-sorted-arrays';
 
 console.log(
   '------------------------------------------------------------------------------'
 );
-const nums = [7, 1, 5, 3, 6, 4];
+const nums1 = [4, 5, 6, 0, 0, 0];
 const val = -1;
-const output = maxProfit(nums);
-console.log(output);
+const output = merge(nums1, 3, [1, 2, 3], 3);
+console.log(nums1);
 console.log(
   '------------------------------------------------------------------------------'
 );
